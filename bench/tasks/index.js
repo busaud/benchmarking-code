4c7de360a42deb4fd1e493bae886308aa0f51ddb
busaud <abdulaziz.almuhaidib.97@gmail.com>
2025-08-12 13:24:21 +0300

    splitting tasks

--- bench/tasks/index.js
+++ bench/tasks/index.js
@@ -3,9 +3,25 @@ const path = require("path");
 
 const tasksDir = __dirname;
 
-const tasks = fs
-    .readdirSync(tasksDir)
-    .filter((f) => f !== "index.js" && f.startsWith("task_") && f.endsWith(".js"))
-    .map((f) => require(path.join(tasksDir, f)));
+function fileNameToId(filename) {
+    const base = filename.replace(/\.js$/i, "");
+    return base.slice(5).replace(/[^a-zA-Z0-9_-]+/g, "_");
+}
 
-module.exports = tasks;
+const collected = [];
+
+for (const file of fs.readdirSync(tasksDir)) {
+    if (file === "index.js") continue;
+    if (!file.endsWith(".js") || !file.startsWith("task_")) continue;
+    const id = fileNameToId(file);
+    if (!id) continue;
+
+    const taskPath = path.join(tasksDir, file);
+    // eslint-disable-next-line import/no-dynamic-require, global-require
+    const task = require(taskPath);
+    if (!task || typeof task !== "object") continue;
+
+    collected.push({ ...task, id });
+}
+
+module.exports = collected;
